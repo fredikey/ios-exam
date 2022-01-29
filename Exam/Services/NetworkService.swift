75df39cc0ccb6e395de329110cd2f680f9626f10
Victor Kozodaev <viktor7778123@gmail.com>
2022-01-29 11:49:22 +0300

    Feat: Network Service (WeatherResponse, Handle Errors)

--- Exam/Services/NetworkService.swift
+++ Exam/Services/NetworkService.swift
@@ -9,11 +9,56 @@ import Foundation
 
 final class NetworkService {
     
+    enum NetworkErrors: Error {
+        case invalidURL
+        case noResponse
+        case serverSideError(Int)
+        case coruptedData
+        case parseError
+    }
+    
     private let apiKey = "29cd1623e161660071d8ec0ce8f0e6c6"
     
-    // MARK: Заменить Result<String, Error> на свою модель и ошибку
-    func getWeather(for city: String, completion: @escaping (Result<String, Error>) -> Void) {
+    // READY: Заменить Result<String, Error> на свою модель и ошибку
+    func getWeather(for city: String, completion: @escaping (Result<WeatherRootResponse, NetworkErrors>) -> Void) {
         
+        guard let url = createURL(city: city) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            // READY: Корректно обработать ошибку, респонс и дату
+            
+            guard let data = data else {
+                completion(.failure(.coruptedData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(.noResponse))
+                return
+            }
+            
+            guard (200...299).contains(response.statusCode) else {
+                completion(.failure(.serverSideError(response.statusCode)))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(WeatherRootResponse.self, from: data)
+                
+                completion(.success(decodedResponse))
+            } catch {
+                completion(.failure(.parseError))
+            }
+
+        }.resume()
+    }
+    
+    private func createURL(city: String) -> URL? {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
         urlComponents.host = "api.openweathermap.org"
@@ -22,26 +67,8 @@ final class NetworkService {
             URLQueryItem(name: "q", value: city),
             URLQueryItem(name: "appid", value: apiKey)
         ]
-
-        guard let url = urlComponents.url else {
-            return
-        }
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            // MARK: Корректно обработать ошибку, респонс и дату
-            
-            guard let data = data else {
-                return
-            }
-
-            guard let string = String(data: data, encoding: .utf8) else {
-                return
-            }
-            
-            completion(.success(string))
-            
-        }.resume()
+        return urlComponents.url
     }
     
 }
